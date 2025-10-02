7c9bb7497e1dd3e5ea8dd41d4b7091324d9c9342
Pat Rooney <sotatech@users.noreply.github.com>
2025-10-02 14:32:36 +0100

    Update Install info

--- webfiles/install.sh
+++ webfiles/install.sh
@@ -20,7 +20,8 @@ sudo netstat -tlnp | grep pigpiod
 
 echo "=== Installing Node-RED ==="
 sudo apt install build-essential git curl
-bash <(curl -sL https://github.com/node-red/linux-installers/releases/latest/download/update-nodejs-and-nodered-deb) // Untested. Check if this actually works.....
+bash <(curl -sL https://github.com/node-red/linux-installers/releases/latest/download/update-nodejs-and-nodered-deb) --confirm-pi --confirm-install
+// Untested. Check if this actually works.....
 
 echo "=== Installing required Node.js modules globally ==="
 # pm2 is optional but useful for autostart
