2f0ba4b86c54ff0d9320adfa0bca7b6d85758129
Pat Rooney <sotatech@users.noreply.github.com>
2026-02-10 17:16:27 +0000

    Update install.sh

--- webfiles/install.sh
+++ webfiles/install.sh
@@ -12,21 +12,22 @@ sudo apt upgrade -y
 echo "=== Installing prerequisites ==="
 sudo apt install -y build-essential mosquitto mosquitto-clients unzip git curl
 
-echo "=== Installing pigpio & confirm status ==="
+#echo "=== Installing pigpio & confirm status ==="
 # sudo apt-get install -y pigpio
-# This package does not come with Debial Trixie, so clone and build pigpio. Not needed for earlier Devian versions.
-sudo apt install -y python3-setuptools python3-full
-git clone https://github.com/joan2937/pigpio.git
-cd pigpio
-make
-sudo make install
-sudo ln -s /usr/local/bin/pigpiod /usr/bin
-sudo cp util/pigpiod.service /etc/systemd/system
-sudo systemctl enable pigpiod
-sudo systemctl start pigpiod
-echo "=== Verify pigpio status ==="
-sudo systemctl status pigpiod
-sudo netstat -tlnp | grep pigpiod
+# This package does not come with Debian Trixie, so clone and build pigpio. Not needed for earlier Debian versions.
+# See here for correct method to manually install pigpiod: https://github.com/joan2937/pigpio/issues/632
+#sudo apt install -y python3-setuptools python3-full
+#git clone https://github.com/joan2937/pigpio.git
+#cd pigpio
+#make
+#sudo make install
+#sudo ln -s /usr/local/bin/pigpiod /usr/bin
+#sudo cp util/pigpiod.service /etc/systemd/system
+#sudo systemctl enable pigpiod
+#sudo systemctl start pigpiod
+#echo "=== Verify pigpio status ==="
+#sudo systemctl status pigpiod
+#sudo netstat -tlnp | grep pigpiod
 
 cd ~
 #echo "=== Installing Node-RED ==="
